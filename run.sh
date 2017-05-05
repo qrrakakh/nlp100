1c55d039c646f792e76baacf375a811b3f2dfdae
qrrakakh (Hiroaki KURAMAE) <hiroaki.kuramae@gmail.com>
2017-05-05 23:36:31 +0900

    automatic data download in run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+
+mkdir -p data/
+wget -q -nc -P data/ http://www.cl.ecei.tohoku.ac.jp/nlp100/data/hightemp.txt
+wget -q -nc -P data/ http://www.cl.ecei.tohoku.ac.jp/nlp100/data/jawiki-country.json.gz
+wget -q -nc -P data/ http://www.cl.ecei.tohoku.ac.jp/nlp100/data/neko.txt
+wget -q -nc -P data/ http://www.cl.ecei.tohoku.ac.jp/nlp100/data/nlp.txt
+wget -q -nc -P data/ http://www.cl.ecei.tohoku.ac.jp/nlp100/data/artist.json.gz
+wget -q -nc -P data/ http://www.cl.ecei.tohoku.ac.jp/nlp100/data/enwiki-20150112-400-r10-105752.txt.bz2
+
 python3 -m unittest tests.test_chap01
 #python3 -m unittest tests.test_chap02
 #python3 -m unittest tests.test_chap03
